3250b8b7a64af0e50622b16be1c8a3f5dc1bcb70
avein <avein@mail.ru>
2020-09-04 22:52:15 +1000

    Добавьте файлы проекта.

---
+++ MyTimer/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace MyTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SetTimer = new System.Windows.Forms.TextBox();
+            this.GetMessage = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SetTimer
+            // 
+            this.SetTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SetTimer.Location = new System.Drawing.Point(12, 12);
+            this.SetTimer.Name = "SetTimer";
+            this.SetTimer.Size = new System.Drawing.Size(156, 29);
+            this.SetTimer.TabIndex = 0;
+            this.SetTimer.Text = "0";
+            this.SetTimer.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // GetMessage
+            // 
+            this.GetMessage.Location = new System.Drawing.Point(12, 47);
+            this.GetMessage.Name = "GetMessage";
+            this.GetMessage.Size = new System.Drawing.Size(156, 33);
+            this.GetMessage.TabIndex = 1;
+            this.GetMessage.Text = "Задать таймер";
+            this.GetMessage.UseVisualStyleBackColor = true;
+            this.GetMessage.Click += new System.EventHandler(this.GetMessage_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(180, 105);
+            this.Controls.Add(this.GetMessage);
+            this.Controls.Add(this.SetTimer);
+            this.Name = "Form1";
+            this.Text = "MyTimer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox SetTimer;
+        private System.Windows.Forms.Button GetMessage;
+    }
+}
+
---
+++ MyTimer/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyTimer
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private Timer timer;
+        
+        private void GetMessage_Click(object sender, EventArgs e)
+        {
+            
+
+            int time = SetTime(SetTimer.Text);
+            if (time == -1) { MessageBox.Show("Некорректное значение"); return; }
+            
+            timer = new Timer();
+            timer.Tick += new EventHandler(ShowMessage);
+            timer.Interval = time;
+            timer.Start();
+            
+            
+        }
+        
+        private int SetTime(string text)
+        {
+            const int sec = 1000;
+            const int min = 60;
+            if (!int.TryParse(text, out int time)) { SetTimer.Text = "0"; return -1; }
+            else return time*sec*min;
+        }
+
+        private async void ShowMessage(object myObject, EventArgs myEventArgs)
+        {
+            TopMost = true;
+            timer.Stop();
+            MessageBox.Show("Время пришло!");
+            TopMost = false;
+        }
+    }
+}
